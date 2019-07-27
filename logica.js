d59abca09bc9e8db989d311cee49aad8e1b340ce
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2019-07-27 17:03:21 -0300

    Calculadora JS

---
+++ logica.js
@@ -0,0 +1,40 @@
+
+var string = '' // Variável da função histórico
+
+function calcular(tipo, valor) {
+	        
+	if(tipo == 'acao') {
+
+	if(valor == 'c') {  // Limpar o visor
+	  document.getElementById('resultado').value = ''
+	}
+
+	if(valor == '+' || valor == '-' || valor == '/'
+	 || valor == '*' || valor == '%' || valor == '.' ) {   // Botões de ação
+	    document.getElementById('resultado').value += valor
+	}
+
+var operacao = document.getElementById('resultado').value  // Salva a operação         
+
+	if(valor == '=') {  // Efetuar o cálculo matemático (eval realiza as operações)
+	  operacao += valor + ' '
+	  var valor_campo = eval(document.getElementById('resultado').value) 
+	  document.getElementById('resultado').value = valor_campo
+	  operacao += valor_campo
+	  historico(operacao)  // Chama Operação com o resultado      
+	}
+
+	} else if (tipo == 'valor') {   // Números no visor
+	   document.getElementById('resultado').value += valor  
+	}
+}
+
+function historico(operacao) { // Função que armazena e exibe o histórico           
+	string += operacao + '\n'	       	
+    document.getElementById('historico').value = string
+}
+
+function limparHistorico() { // Função que limpa o histórico          
+	string = ''       	
+    document.getElementById('historico').value = string
+}
