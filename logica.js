4cefa89612a788f7dfff7925f4bb2a5082d39578
Samuel-Lucas <49723833+Samuel-Lucas@users.noreply.github.com>
2019-07-28 16:01:18 -0300

    Erro 'undefined' corrigido

--- logica.js
+++ logica.js
@@ -14,14 +14,20 @@ function calcular(tipo, valor) {
 	    document.getElementById('resultado').value += valor
 	}
 
-var operacao = document.getElementById('resultado').value  // Salva a operação         
+var operacao = document.getElementById('resultado').value  // Salva a operação para depois ser exibida no histórico        
 
 	if(valor == '=') {  // Efetuar o cálculo matemático (eval realiza as operações)
-	  operacao += valor + ' '
-	  var valor_campo = eval(document.getElementById('resultado').value) 
-	  document.getElementById('resultado').value = valor_campo
-	  operacao += valor_campo
-	  historico(operacao)  // Chama Operação com o resultado      
+		
+	    operacao += valor + ' '
+	    var valor_campo = eval(document.getElementById('resultado').value)
+
+	  if(valor_campo == null) {
+	  	document.getElementById('resultado').value = 0
+	  } else {
+	    document.getElementById('resultado').value = valor_campo
+	    operacao += valor_campo
+	    historico(operacao)  // Chama Operação com o resultado    
+	  }  
 	}
 
 	} else if (tipo == 'valor') {   // Números no visor
